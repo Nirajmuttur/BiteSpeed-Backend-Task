15a62d5a659141f9ce453ed7b504d4f3b6903cd2
nirajmuttur <nirajmuttur@gmail.com>
2024-06-09 12:01:09 +0530

    removed swagger in routes

--- src/routes/identityRoutes.ts
+++ src/routes/identityRoutes.ts
@@ -5,53 +5,6 @@ import { identitySchema } from '../validationSchema/identitySchema';
 const router=express.Router();
 
 
-/**
- * @swagger
- * components:
- *   schemas:
- *     Identity:
- *       type: object
- *       properties:
- *         primaryContactId:
- *           type: array
- *         emails: 
- *           type: array
- *         phoneNumbers:
- *           type: array
- *         secondaryContactIds:
- *           type: array
- */
-
-/**
- * @swagger
- * /api/identity:
- *   post:
- *     tags: ['Identity']
- *     description: Create new idenity
- *     consumes: 
- *     - application/json
- *     responses:
- *       400:
- *         description: Bad request, invalid input
- *       500:
- *         description: Internal server error
- *       200:
- *         description: Succssfull Response
- *         content:
- *           application/json:
- *             schema:
- *               $ref: '#components/schemas/Identity'
- *     requestBody:
- *       content:
- *         application/json:
- *           schema:
- *             type: object
- *             properties:
- *               email:
- *                 type: string
- *               phoneNumber:
- *                 type: number
- */
 router.route('/identity').post(createIdentityValidation(identitySchema),createIdentity);
 
 export default router
